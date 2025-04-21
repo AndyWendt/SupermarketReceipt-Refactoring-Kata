8fe58f5f7354b7d7504956f91d50e5683be7f35b
Andy Wendt <andy@awendt.com>
2025-04-21 16:11:38 -0600

    Add test for item presentation

--- php/tests/ReceiptPrinterTest.php
+++ php/tests/ReceiptPrinterTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests;
 
+use Supermarket\Model\Product;
+use Supermarket\Model\ProductUnit;
 use Supermarket\Model\Receipt;
+use Supermarket\Model\ReceiptItem;
 use Supermarket\ReceiptLineItem;
 use Supermarket\ReceiptPrinter;
 use PHPUnit\Framework\TestCase;
@@ -16,6 +19,26 @@ class ReceiptPrinterTest extends TestCase
         $instance = new ReceiptPrinter();
 
         $line = (new ReceiptLineItem(columns: 40))->formatLine(name: 'Total:', value: '0.00');
-        $this->assertSame("\n$line", $instance->printReceipt($receipt));
+        $result = $instance->printReceipt($receipt);
+
+        $this->assertSame("\n$line", $result);
+    }
+
+    public function test_it_presents_items()
+    {
+        $product = new Product('Foo', ProductUnit::EACH());
+
+        $receipt = new Receipt();
+        $receipt->addProduct(product: $product, quantity: 5, price: 5.00, totalPrice: 25.00);
+        $receipt->addProduct(product: $product, quantity: 1, price: 1000.00, totalPrice: 1000.00);
+        $instance = new ReceiptPrinter();
+
+        $result = $instance->printReceipt($receipt);
+
+        $this->assertSame('Foo                                25.00
+  5.00 * 5
+Foo                              1000.00
+
+Total:                           1025.00', $result);
     }
 }
