84e11714de53291ba2f98acb73b34b7d4400cb23
Andy Wendt <andy@awendt.com>
2025-04-21 15:51:53 -0600

    R remove duplication in test

--- php/tests/ReceiptPrinterTest.php
+++ php/tests/ReceiptPrinterTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Supermarket\Model\Receipt;
+use Supermarket\ReceiptLineItem;
 use Supermarket\ReceiptPrinter;
 use PHPUnit\Framework\TestCase;
 
@@ -14,14 +15,7 @@ class ReceiptPrinterTest extends TestCase
 
         $instance = new ReceiptPrinter();
 
-        $this->assertSame('
-Total:                              0.00', $instance->printReceipt($receipt));
-    }
-
-    public function test_it_formats_a_line_item()
-    {
-        $instance = new ReceiptPrinter();
-
-        $this->assertSame('Total:                              0.00', $instance->formatLineWithWhiteSpace('Total:', '0.00'));
+        $line = new ReceiptLineItem(40, 'Total:', '0.00');
+        $this->assertSame("\n$line", $instance->printReceipt($receipt));
     }
 }
